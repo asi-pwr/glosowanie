aa4c4fc38218d142030bb1a121eb74f65e8ff446
Michał Zając <michal.zajac@gmail.com>
2017-06-05 19:20:20 +0200

    GMail pls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "student.pwr.wroc.pl",
-    :port => "143",
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
     :user_name => Rails.application.secrets.email_username,
     :password => Rails.application.secrets.email_password,
     :authentication => "plain",
